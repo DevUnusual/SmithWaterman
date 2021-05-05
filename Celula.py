8f8f1ae93e85e54b70ac28ec860e372dd3f5701f
DevUnusual <carlos.meneses.g.s@gmail.com>
2021-05-05 00:39:56 -0300

    matriz

--- Celula.py
+++ Celula.py
@@ -1,13 +1,7 @@
-class celula:
-  def __init__(self, A, B):
-    celulaAbaixo = []
-    if A[2] == 0 or B[2] ==0:
-      if A[2]!=0:
+# class celula:
+#   def __init__(self, A, B):
+#     CelEsq = []
+#     CelDir = []
+#     CelDiagonal = []
 
-
-    celulaAesquerda = []
-
-    caminhos = []
-
-    print("celula criada")
-  
+#     print("criando")
--- Smith.py
+++ Smith.py
@@ -1,8 +1,22 @@
 import GapCalculator as gap
-from Celula import celula
+# from Celula import celula
 
 dnaA = "tgcccaagtgctgccaaa"
 dnaB = "gcctagtccatgatggta"
 dnaA = gap.start(dnaA)
 dnaB = gap.start(dnaB)
 
+def Criarmatriz(a, b):
+  temporario = []
+  matriz = []
+  for i in a:
+    print("i" + str(i))
+    for j in b:
+      print('j'+ str(j))
+      temporario.append(i + j)
+    # print(temporario)
+    matriz.append(temporario)
+    temporario = []
+  return matriz
+table = Criarmatriz(dnaA, dnaB)
+print(table)
