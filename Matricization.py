a7586e8b3d4a0d2f5bb6b75791ecf1a24ea884ed
DevUnusual <carlos.meneses.g.s@gmail.com>
2021-05-14 01:25:27 -0300

    removida cellaround e 3 funcoes

--- Matricization.py
+++ Matricization.py
@@ -12,31 +12,29 @@ class Criarmatriz():
     if posA == posB == 0: return CellAround[0,0,0]
     if posA >=1:
       if posB >=1:
-        CellAround.append(self.matriz[posA, (posB-1)]) #esquerda
-        CellAround.append(self.matriz[(posA-1), posB]) #vertical
-        CellAround.append(self.matriz[(posA-1), (posB-1)]) #diagonal
+        CellAround.append(1) #esquerda
+        CellAround.append(1) #vertical
+        CellAround.append(1) #diagonal
       else:
         CellAround.append(0) #horizontal
-        CellAround.append(self.matriz[(posA-1), posB]) #vertical
+        CellAround.append(1) #vertical
         CellAround.append(0) #diagonal
     else:
-        CellAround.append(self.matriz[posA, (posB-1)]) #esquerda
+        CellAround.append(1) #esquerda
         CellAround.append(0) #vertical
         CellAround.append(0) #diagonal
 
     return CellAround
 
-  def calculateHorizontal():
+  def calculateAround(parametros):
+    #calcula as celulas ao redor volta o maximo e o caminho para chegar no maximo
 
-  def calculateVertical():
-  def calculateDiagonal():
 
   def makeMatrizFromList(self):
-    CellAround =[0,0,0] #é para guardar as celulas ao redor se existir
     LinesForMatriz = []
     for i in self.list_a:
       for j in self.list_b:
-        CellAround = self.cellAroundExist(i[2],j[2])
+        self.calculateAround(self.cellAroundExist(i[2],j[2])) #volta a lista de celulas ao redor e calcula o maior
         #calcular os valores para definir o maior valor
 
 
