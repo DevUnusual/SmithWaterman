1ecba071e62896dcf6e8b53afd5748fb47bf77e6
DevUnusual <carlos.meneses.g.s@gmail.com>
2021-05-14 17:32:28 -0300

    celula atributo

--- Celula.py
+++ Celula.py
@@ -3,5 +3,5 @@ class celula:
   def __init__(self, A, B, parametros):
     self.letterA = A[0]
     self.letterB = B[0]
-    self.Max = parametros[0]
-    self.posicao = [parametros[1], parametros[2]]
+    self.Max = [parametros[0], parametros[1]]
+    self.posicao = [parametros[2], parametros[3]]
--- Matricization.py
+++ Matricization.py
@@ -5,6 +5,9 @@ class Criarmatriz():
     self.list_a = list(a)
     self.list_b = list(b)
     self.matriz =[]
+    self.match = parametros[0]
+    self.mismatch= parametros[1]
+    self.gap = parametros[2]
     self.makeMatrizFromList()
 
   def cellAroundExist(self, posA, posB):
@@ -12,29 +15,33 @@ class Criarmatriz():
     if posA == posB == 0: return CellAround[0,0,0]
     if posA >=1:
       if posB >=1:
-        CellAround.append(1) #esquerda
-        CellAround.append(1) #vertical
-        CellAround.append(1) #diagonal
+        CellAround.append(self.matriz[posA-1, posB]) #esquerda
+        CellAround.append(self.matriz[posA, posB-1]) #vertical
+        CellAround.append(self.matriz[posA-1, posB-1]) #diagonal
       else:
         CellAround.append(0) #horizontal
-        CellAround.append(1) #vertical
+        CellAround.append(self.matriz[posA, posB-1]) #vertical
         CellAround.append(0) #diagonal
     else:
-        CellAround.append(1) #esquerda
+        CellAround.append(self.matriz[posA-1, posB]) #esquerda
         CellAround.append(0) #vertical
         CellAround.append(0) #diagonal
 
     return CellAround
 
-  def calculateAround(parametros):
+  def calculateAround(parametros, posA, posB):
     #calcula as celulas ao redor volta o maximo e o caminho para chegar no maximo
-
+  if parametros[0] != 0:
+    esquerda = self.matriz[]
+  if parametros[1] != 0:
+  if parametros[2] != 0:
 
   def makeMatrizFromList(self):
+    resultado = [] #vai contar max e caminho [maximo,[caminhos(-/|)]]
     LinesForMatriz = []
     for i in self.list_a:
       for j in self.list_b:
-        self.calculateAround(self.cellAroundExist(i[2],j[2])) #volta a lista de celulas ao redor e calcula o maior
+        resultado = self.calculateAround(self.cellAroundExist(i[2],j[2]), i, j) #volta a lista de celulas ao redor e calcula o maior
         #calcular os valores para definir o maior valor
 
 
