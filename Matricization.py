8e092253abac8372b9a18c5d3ff437c7acec8fdd
DevUnusual <carlos.meneses.g.s@gmail.com>
2021-05-18 01:46:39 -0300

    tem que concertar ainda

--- Matricization.py
+++ Matricization.py
@@ -9,6 +9,9 @@ class Criarmatriz():
     self.mismatch= parametros[1]
     self.gap = parametros[2]
     self.makeMatrizFromList()
+  
+  def getCellMatriz(self, a, b):
+    return self.matriz[a][b]
 
   def cellAroundExist(self, posA, posB):
     CellAround =[] #é para guardar as celulas [horizontal, vertical, diagonal]
@@ -16,15 +19,15 @@ class Criarmatriz():
       return [0,0,0]
     if posA >=1:
       if posB >=1:
-        CellAround.append(self.matriz[posA-1, posB]) #esquerda
-        CellAround.append(self.matriz[posA, posB-1]) #vertical
-        CellAround.append(self.matriz[posA-1, posB-1]) #diagonal
+        CellAround.append(self.getCellMatriz(posA-1, posB)) #esquerda
+        CellAround.append(self.getCellMatriz(posA, posB-1)) #vertical
+        CellAround.append(self.getCellMatriz(posA-1, posB-1)) #diagonal
       else:
         CellAround.append(0) #horizontal
-        CellAround.append(self.matriz[posA, posB-1]) #vertical
+        CellAround.append(self.getCellMatriz(posA, posB-1)) #vertical
         CellAround.append(0) #diagonal
     else:
-        CellAround.append(self.matriz[posA-1, posB]) #esquerda
+        CellAround.append(self.getCellMatriz(posA-1, posB)) #esquerda
         CellAround.append(0) #vertical
         CellAround.append(0) #diagonal
 
@@ -37,11 +40,10 @@ class Criarmatriz():
       esquerda = parametros[0].getMax() + self.gap
     if parametros[1] != 0:
       vertical = parametros[1].getMax() + self.gap
-    if parametros[2] != 0:
-      if self.list_a[posA] == self.list_b[posB]:
-        diagonal = parametros[2].getMax() + self.match
-      else:
-        diagonal = parametros[2].getMax() + self.mismatch
+    if self.list_a[posA] == self.list_b[posB]:
+      diagonal = parametros[2].getMax() + self.match
+    else:
+      diagonal = parametros[2].getMax() + self.mismatch
     resultados = [esquerda,vertical,diagonal]     
     resultados.sort(reverse= True)
     caminho = []
@@ -55,15 +57,13 @@ class Criarmatriz():
 
   def makeMatrizFromList(self):
     resultado = [] #vai contar max e caminho [maximo,[caminhos(-/|)]]
-    LinesForMatriz = []
     for i in self.list_a:
       for j in self.list_b:
-        resultado = self.calculateAround((self.cellAroundExist(i[2],j[2])), i, j) #volta a lista de celulas ao redor e calcula o maior
+        resultado = self.calculateAround((self.cellAroundExist(i[2],j[2])), i[2], j[2]) #volta a lista de celulas ao redor e calcula o maior
         resultado.append(i[2])
         resultado.append(j[2])
-        LinesForMatriz.append(celula(i[0],j[0],resultado))
-      self.matriz.append(LinesForMatriz)
-      LinesForMatriz = []
+        self.matriz[i[2]].append(celula(i[0],j[0],resultado))
+      self.matriz.append(list)
         #calcular os valores para definir o maior valor
 
 
